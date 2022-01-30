e09404a2ee2115c79515e8218c390e231cc0a1e9
unknown <gustavoc.moraes@hotmail.com>
2022-01-30 18:55:01 -0300

    feat: added celebrate to routes

--- node/src/modules/users/routes.js
+++ node/src/modules/users/routes.js
@@ -6,40 +6,50 @@ import { celebrate, Joi, Segments } from 'celebrate';
 
 const userRoutes = Router();
 
-const createUserController =  new CreateUserController();
-const authenticateUserController =  new AuthenticateUserController();
+const createUserController = new CreateUserController();
+const authenticateUserController = new AuthenticateUserController();
 
-userRoutes.post('/', 
-celebrate({
-  [Segments.BODY]: {
-    name: Joi.string().required(),
-    email: Joi.string().email().required(),
-    password: Joi.string(),
-    password_confirmation: Joi.string().valid(Joi.ref('password')),
-  },
-}), createUserController.create);
+userRoutes.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string(),
+      password_confirmation: Joi.string().valid(Joi.ref('password')),
+    },
+  }),
+  createUserController.create
+);
 
-userRoutes.post('/authenticate', celebrate({
-  [Segments.BODY]: {
-    email: Joi.string().email().required(),
-    password: Joi.string().required(),
-  },
-}), authenticateUserController.handle);
+userRoutes.post(
+  '/authenticate',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  authenticateUserController.handle
+);
 
 userRoutes.use(ensureAuthenticated);
 
 userRoutes.delete('/', createUserController.destroy);
 userRoutes.get('/', createUserController.show);
 
-userRoutes.put('/', celebrate({
-  [Segments.BODY]: {
-    name: Joi.string().required(),
-    email: Joi.string().email().required(),
-    old_password: Joi.string(),
-    password: Joi.string(),
-    password_confirmation: Joi.string().valid(Joi.ref('password')),
-  },
-}), createUserController.update);
-
+userRoutes.put(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      old_password: Joi.string(),
+      password: Joi.string(),
+      password_confirmation: Joi.string().valid(Joi.ref('password')),
+    },
+  }),
+  createUserController.update
+);
 
 export { userRoutes };
