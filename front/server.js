ec4c24150f6c507a7c525e9c961457e27486bfb5
lucasrbasso <lucas.rbasso@outlook.com>
2022-02-03 19:47:42 -0300

    feat: new web structure

---
+++ front/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path = require('path')
+const app = express();
+
+app.use(express.static('./src'));
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "src", 'pages',"login.html"));
+});
+
+app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "src", 'pages', "index.html"));
+});
+
+app.listen(3000, () => console.log('Projeto rodando na porta 3000...'));
