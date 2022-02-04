279ec81e2cc4de36e242bf41ccbb5bff28f7a642
lucasrbasso <lucas.rbasso@outlook.com>
2022-02-04 01:00:58 -0300

    feat: user session

--- front/server.js
+++ front/server.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.use(express.static('./src'));
 
+app.get("/register", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "src", 'pages',"register.html"));
+});
+
 app.get("/login", (req, res) => {
   res.sendFile(path.resolve(__dirname, "src", 'pages',"login.html"));
 });
@@ -12,4 +16,4 @@ app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "src", 'pages', "index.html"));
 });
 
-app.listen(3000, () => console.log('Projeto rodando na porta 3000...'));
+app.listen(3333, () => console.log('Projeto rodando na porta 3333...'));
---
+++ front/src/utils/redirectToIndexIfLogged.js
@@ -0,0 +1,41 @@
+const validateUserLogged = async () => {
+
+  const token = localStorage.getItem('@js-quiz:token');
+  const user = localStorage.getItem('@js-quiz:user');
+
+  if (!token && !user) {
+    localStorage.removeItem('@js-quiz:user');
+    localStorage.removeItem('@js-quiz:token');
+  }
+  
+  try {
+    const req = await fetch('http://localhost:3000/users', {
+      method: 'GET',
+      headers: { 
+        'Authorization': `Bearer ${token}`,
+        'Content-Type':'application/json' 
+      },
+    });
+  
+    await req.json();
+  
+    if(req.ok) {
+
+      window.location.replace('/');
+      return;
+  
+    } else {
+      localStorage.clear('@js-quiz:user');
+      localStorage.clear('@js-quiz:token');
+    }
+    
+  } catch(err) {
+    localStorage.clear('@js-quiz:user');
+    localStorage.clear('@js-quiz:token');
+  }
+}
+
+validateUserLogged();
+
+
+
