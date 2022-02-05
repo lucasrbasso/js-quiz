9a78f0faba23b9e6198dfe2a7f1ca740f87e3419
lucasrbasso <lucas.rbasso@outlook.com>
2022-02-05 18:18:38 -0300

    feat: logout and back button

--- front/src/scripts/profile.js
+++ front/src/scripts/profile.js
@@ -5,16 +5,34 @@ toastr.options.progressBar = true;
 toastr.options.closeDuration = 300;
 toastr.options.closeButton = true;
 
+const user = {};
+
+document.querySelectorAll('#user-form [name]')
+.forEach(input => user[input.name] = input);
+
+const { name, email, old_password, password, password_confirmation } = user;
+
+try {
+  const req = await fetch('http://localhost:3000/users', {
+    method: 'GET',
+    headers: { 
+      'Authorization': `Bearer ${token}`,
+      'Content-Type':'application/json' 
+    },
+  });
+
+  const response = await req.json();
+
+  if(req.ok) {
+    name.value = response.name;
+    email.value = response.email;
+  }
+} catch(err) {
+  toastr.error('Tente novamente mais tarde.', 'Problemas ao carregar perfil!');
+}
+
 const updateUser = async (event) => {
   event.preventDefault();
-
-  const user = {};
-
-  document.querySelectorAll('#user-form [name]')
-    .forEach(input => user[input.name] = input);
-
-  const { name, email, old_password, password, password_confirmation } = user;
-
 
   if(!validate(name, email, old_password, password, password_confirmation)) {
     return;
@@ -141,8 +159,17 @@ const deleteUser = () => {
   })
 }
 
+const logout = () => {
+  localStorage.removeItem('@js-quiz:user');
+  localStorage.removeItem('@js-quiz:token');
+  window.location.replace('/');
+}
+
 const submitButton = document.getElementById('submitButton');
 submitButton.addEventListener('click',updateUser);
 
 const deleteButton = document.getElementById('deleteButton');
 deleteButton.addEventListener('click',deleteUser);
+
+const logoutButton = document.getElementById('logout');
+logoutButton.addEventListener('click',logout);
