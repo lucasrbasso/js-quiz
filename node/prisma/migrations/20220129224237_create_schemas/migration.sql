1bda3aac541295895c27f274187280cec1db8f9f
Rodrigo Moraes <rodrigomoraes61@outlook.com>
2022-02-15 16:40:41 -0300

    migration just correct

--- node/prisma/migrations/20220129224237_create_schemas/migration.sql
+++ node/prisma/migrations/20220129224237_create_schemas/migration.sql
@@ -4,8 +4,6 @@
   - You are about to drop the `user` table. If the table is not empty, all the data it contains will be lost.
 
 */
--- DropTable
-DROP TABLE `user`;
 
 -- CreateTable
 CREATE TABLE `users` (
@@ -23,13 +21,13 @@ CREATE TABLE `topics` (
     `id` VARCHAR(191) NOT NULL,
     `description` VARCHAR(191) NOT NULL,
     `image_url` VARCHAR(191) NOT NULL,
-    `tite` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `quizes` (
+CREATE TABLE `quizzes` (
     `id` VARCHAR(191) NOT NULL,
     `topic_id` VARCHAR(191) NOT NULL,
     `title` VARCHAR(191) NOT NULL,
@@ -63,10 +61,10 @@ CREATE TABLE `answers` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `quizes` ADD CONSTRAINT `quizes_topic_id_fkey` FOREIGN KEY (`topic_id`) REFERENCES `topics`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `quizzes` ADD CONSTRAINT `quizzes_topic_id_fkey` FOREIGN KEY (`topic_id`) REFERENCES `topics`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `questions` ADD CONSTRAINT `questions_quiz_id_fkey` FOREIGN KEY (`quiz_id`) REFERENCES `quizes`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `questions` ADD CONSTRAINT `questions_quiz_id_fkey` FOREIGN KEY (`quiz_id`) REFERENCES `quizzes`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `answers` ADD CONSTRAINT `answers_question_id_fkey` FOREIGN KEY (`question_id`) REFERENCES `questions`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
